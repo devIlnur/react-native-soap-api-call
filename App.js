b94bb5a53b90c45879c3ab01b8c9ca7a66ae383d
Dev <ilnuryalalet@gmail.com>
2018-06-23 06:48:04 +0300

    removed unused code

--- App.js
+++ App.js
@@ -118,17 +118,7 @@ export default class App extends Component<Props> {
   
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <View style={styles.container} />
     );
   }
 }
@@ -139,15 +129,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
